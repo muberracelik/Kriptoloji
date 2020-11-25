7df7702022c19ceeb4a1d1c20f709f063dfbd60d
Taha Batuhan Türk <tbturkk@gmail.com>
2020-11-25 12:09:52 +0300

    hatalar giderildi sorun çözümlendi

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> elemanlar = new ArrayList<>();
+        ArrayList<String> hexler = new ArrayList<>();
+        ArrayList<String> polinomHexler = new ArrayList<>();
+        polinomHexler.add("0");
         Scanner sc = new Scanner(System.in); //System.in is a standard input stream  
         System.out.print("İndirgenemez polinomu giriniz ör(x4+x+1): ");
         String polinom = sc.nextLine();              //reads string   
@@ -35,16 +38,58 @@ public class Main {
         } else {
             sagTaraf = sagsol[0] + sagsol[1];
         }
-        System.out.println(sagTaraf);
         int n = (int) Math.pow(2, enB) - 1;
-        System.out.println(n);
+
+        for (int i = 0; i < n + 1; i++) {
+            String hexa = Integer.toHexString(i);
+            hexler.add(hexa);
+        }
         elemanlar.add("a");
-        System.out.println("a^1= a");
+        String binary1 = "";
+        String[] bolunmus1 = "a".split("\\+");
+        for (int j = binaryler.size() - 1; j >= 0; j--) {
+            boolean varmi = false;
+            for (int k = 0; k < bolunmus1.length; k++) {
+
+                if (bolunmus1[k].equals(binaryler.get(j))) {
+                    varmi = true;
+                }
+            }
+            if (varmi == true) {
+                binary1 += "1";
+            } else {
+                binary1 += "0";
+            }
+        }
+        int num1 = Integer.parseInt(binary1, 2);
+        String hexa1 = Integer.toHexString(num1);
+        System.out.println("\na^1= a" + " (" + binary1 + "-" + hexa1 + ")");
+        polinomHexler.add(hexa1);
         for (int i = 1; i < n; i++) {
             int tmp = i;
             if (tmp == enB - 1) {
                 elemanlar.add(sagTaraf);
-                System.out.println("a^" + enB + "= " + elemanlar.get(elemanlar.size() - 1));
+                String binary = "";
+                String[] bolunmus = sagTaraf.split("\\+");
+                for (int j = binaryler.size() - 1; j >= 0; j--) {
+                    boolean varmi = false;
+                    for (int k = 0; k < bolunmus.length; k++) {
+
+                        if (bolunmus[k].equals(binaryler.get(j))) {
+                            varmi = true;
+                        }
+                    }
+                    if (varmi == true) {
+                        binary += "1";
+                    } else {
+                        binary += "0";
+                    }
+                }
+                int num = Integer.parseInt(binary, 2);
+                String hexa = Integer.toHexString(num);
+                System.out.println("a^" + enB + "= " + elemanlar.get(elemanlar.size() - 1) + " (" + binary + "-" + hexa + ")");
+                polinomHexler.add(hexa);
+
             } else {
                 String[] artiSplit = elemanlar.get(i - 1).split("\\+");
                 String eleman = "";
@@ -108,11 +153,38 @@ public class Main {
                 }
                 int num = Integer.parseInt(binary, 2);
                 String hexa = Integer.toHexString(num);
-                System.out.println("a^" + (i + 1) + "= " + eleman + " " + binary+" "+hexa);
-
+                System.out.println("a^" + (i + 1) + "= " + eleman + " (" + binary + "-" + hexa + ")");
+                polinomHexler.add(hexa);
             }
         }
 
+        System.out.print("Haritalamayı Giriniz: x->x^");
+        int haritalama = sc.nextInt(); // kullanıcının girdiği integer haritalama değerini alır.        
+        String[] cikisHexa = new String[hexler.size()];
+        for (int i = 0; i < cikisHexa.length; i++) {
+            cikisHexa[i] = "0";
+        }
+        for (int i = 1; i < hexler.size(); i++) {
+            int moduAlinmis;
+            moduAlinmis = Math.floorMod(i * haritalama, n);
+
+            if (moduAlinmis == 0) {
+                cikisHexa[Integer.parseInt(polinomHexler.get(i), 16)] = polinomHexler.get(n);
+            } else {
+                cikisHexa[Integer.parseInt(polinomHexler.get(i), 16)] = polinomHexler.get(moduAlinmis);
+            }
+        }
+
+        System.out.print("Giriş:");
+        for (int i = 0; i < hexler.size(); i++) {
+            System.out.print("\t" + hexler.get(i).toUpperCase());
+        }
+        System.out.println("");
+        System.out.print("Çıkış:");
+        for (int i = 0; i < cikisHexa.length; i++) {
+            System.out.print("\t" + cikisHexa[i].toUpperCase());
+        }
+        System.out.println("");
     }
 
 }
